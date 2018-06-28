d1dd14a0c1bdd7a7a512c09cf952d6d88861b3bb
Ayo <mail@ayoayco.com>
2018-06-28 17:10:09 +0800

    initial commit

---
+++ src/app/Github/Github.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Github.css';
+
+export class Github extends Component {
+    render() {
+        return (
+            <div className="Github">
+                <h1>Github Cards</h1>
+                <p>Some people from Github</p>
+            </div>
+        );
+    }
+}
+
