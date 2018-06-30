27a65df8a8f43c1a0e6714247de3c30497bedf8b
Ayo <mail@ayoayco.com>
2018-07-01 01:09:49 +0800

    update description

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <p>This will contain apps created by Ayo for learning React.js conepts.</p>
         </header>
         <Calc />
         <Github />
